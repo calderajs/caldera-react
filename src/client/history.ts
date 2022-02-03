116d940701fb398a99aaddd0aebc4275f728dce6
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2022-02-03 15:09:29 +0800

    [upgrade][wip][package] history upgrade

--- src/client/history.ts
+++ src/client/history.ts
@@ -19,7 +19,7 @@ let unsubscribeListener: (() => void) | null = null;
 
 export const registerHistoryListener = () => {
   if (!unsubscribeListener) {
-    unsubscribeListener = history.listen((location, action) => {
+    unsubscribeListener = history.listen(({ location, action }) => {
       dispatchEvent({
         event: EventType.HISTORY_EVENT,
         action: actionEnumMapping[action],
